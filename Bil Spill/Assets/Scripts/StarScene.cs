1be6e981d3d971271ea9de589e71fb4f04d839ff
ErikBekkemellem <erik.bekkemellem@gmail.com>
2024-04-26 21:50:03 +0200

    eeee

---
+++ Bil Spill/Assets/Scripts/StarScene.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StarScene : MonoBehaviour
+{
+    void Update()
+    {
+
+        if (Input.GetButton("Restart1") || Input.GetButton("Restart2"))
+        {
+            SceneManager.LoadScene("SimplePoly City - Low Poly Assets_Demo Scene");
+        }
+    }
+}
